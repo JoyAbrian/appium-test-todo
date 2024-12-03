81f01a03e9f995f66defb707d6071ebaf612e91b
anisanhy <joyabrianrantepasang14@gmail.com>
2024-12-04 00:20:41 +0800

    update env

--- utils/appium_setup.py
+++ utils/appium_setup.py
@@ -1,13 +1,19 @@
 from appium import webdriver
+from dotenv import load_dotenv
+import os
 
+load_dotenv()
 def get_driver():
     desired_capabilities = {
         "platformName": "Android",
-        "platformVersion": "11.0",
-        "deviceName": "emulator-5554",
+        "platformVersion": os.getenv("PLATFORM_VERSION"),
+        "deviceName": os.getenv("DEVICE_NAME"),
         "app": "app/app-debug.apk",
         "automationName": "UiAutomator2"
     }
-    driver = webdriver.Remote("http://127.0.0.1:4723/wd/hub", desired_capabilities)
-    driver.implicitly_wait(10)
+    appium_server = os.getenv("APPIUM_SERVER")
+    implicit_wait = 10
+    
+    driver = webdriver.Remote(appium_server, desired_capabilities)
+    driver.implicitly_wait(implicit_wait)
     return driver
